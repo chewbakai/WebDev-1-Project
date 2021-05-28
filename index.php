e2b32e2cf144c79159eb92e475d351ae57235ee8
kaiceniza <kaiceniza@users.noreply.github.com>
2021-05-28 23:18:47 +0800

    project-added

---
+++ index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <title>Tuteender</title>
+    
+    <link rel="stylesheet" href="css/bootstrap.css" />
+    <link rel="stylesheet" href="css/flaticon.css" />
+    <link rel="stylesheet" href="css/themify-icons.css" />
+    <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css" />
+    <link rel="stylesheet" href="vendors/nice-select/css/nice-select.css" />
+   
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+
+  <body>
+  
+    <header class="header_area">
+      <div class="main_menu">
+        <div class="search_input" id="search_input_box">
+          <div class="container">
+            <form class="d-flex justify-content-between" method="" action="">
+              <input
+                type="text"
+                class="form-control"
+                id="search_input"
+                placeholder="Looking for something? A specific tutor? A specific topic? Put it in here!"
+              />
+              <button type="submit" class="btn"></button>
+              <span
+                class="ti-close"
+                id="close_search"
+                title="Close Search"
+              ></span>
+            </form>
+          </div>
+        </div>
+
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="container">
+          
+            <a class="navbar-brand logo_h" href="tutor.html"
+              ><img src="img/logoteender.png" alt=""
+            /></a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="icon-bar"></span> <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            
+            <div
+              class="collapse navbar-collapse offset"
+              id="navbarSupportedContent"
+            >
+              <ul class="nav navbar-nav menu_nav ml-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="index.html">Home</a>
+                </li>
+               
+                <li class="nav-item">
+                  <a class="nav-link" href="tutor.html"
+                    >Tutors</a>
+                  </li>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+  
+    <section class="home_banner_area">
+      <div class="banner_inner">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="banner_content text-center">
+                <p class="text-uppercase">
+                  A place to seek and lend help
+                </p>
+                <h2 class="text-uppercase mt-4 mb-5">
+                  SHARE KNOWLEDGE AND SAVE LIVES
+                </h2>
+                <div>
+                  <a href="register.html" class="primary-btn2 mb-3 mb-sm-0">Sign Up</a>
+                  <a href="login.html" class="primary-btn ml-sm-3 ml-0">Log in </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer class="footer-area">   
+
+    <section class="about_area section_gap">
+        <div class="container">
+          <div class="row h_blog_item">
+            
+              <div class="h_blog_text">
+                  <div class="h_blog_text_inner">
+                    
+                    <h4>Welcome to Tuteender</h4>
+                    <p>
+                      Tuteender was a project made by Kyrah Ceniza and Kirk Poro
+                      back in 2019. They were both second year college students in
+                      Information Technology and Computer Science, respectively
+                    </p>
+                    <p>
+                      Tuteender was created and was thought of by the creators as
+                      the courses they had undergone, were by no means, easily
+                      self-taught through the countless tutorials and lessons that
+                      were found on the internet, as programming, in essence does
+                      not have a structured nor a definite way.
+                    </p>
+                    
+                  </div>
+                </div>
+              
+
+                    <div class="single_feature">
+                      <div class="icon"><span class="flaticon-student"></span></div>
+                      <div class="desc">
+                        <h4 class="mt-3 mb-2">Quality Tutors </h4>
+                        <p>
+                          We ensure you that our tutors are carefully handpicked by our team
+                          who reviews and constantly inspects our tutors so that we can provide
+                          you the best and safest way to learn possible.
+                        </p>
+                      </div>
+                    </div>
+                 
+
+                      <div class="single_feature">
+                          <div class="icon"><span class="flaticon-book"></span></div>
+                          <div class="desc">
+                            <h4 class="mt-3 mb-2">Great Learning Experience!</h4>
+                            <p>
+                              Before we had this project, we had a research group who had tried the experience
+                              we would want to provide and had an astounding 95% success rate in terms of tutors and tutee
+                              satisfaction.
+                            </p>
+                          </div>
+                        </div>
+                      
+
+                      <div class="single_feature">
+                          <div class="icon"><span class="flaticon-earth"></span></div>
+                          <div class="desc">
+                            <h4 class="mt-3 mb-2">Global Learning</h4>
+                            <p>
+                              As our tutors provide hands-on and quality learning, this is also a way
+                              on how users could potentially collaborate with other users connecting people
+                              with the same interests.
+                            </p>
+                          </div>
+                       </div>
+
+                       <div class="desc">
+                         <br>
+                         <br>
+                          <p>All Rights Reserved 2019</p>
+                        </div>
+                      
+        </div>
+      </section>
+      
+      
+    </footer>
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="vendors/nice-select/js/jquery.nice-select.min.js"></script>
+    <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+    <script src="js/owl-carousel-thumb.min.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/mail-script.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+    <script src="js/gmaps.min.js"></script>
+    <script src="js/theme.js"></script>
+  </body>
+</html>
